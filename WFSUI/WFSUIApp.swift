ef6924e26edb1bbed0eaacba10eead849885c53c
Gerald Oluoch <94605284s@gmail.com>
2025-05-24 17:09:08 +0300

    Initial Commit

---
+++ WFSUI/WFSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WFSUIApp.swift
+//  WFSUI
+//
+//  Created by Gerald Oluoch on 24/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct WFSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
