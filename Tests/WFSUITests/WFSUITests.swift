0cd0ed26b9eeb139118928c0273c085bc0719a2c
Gerald Oluoch <94605284s@gmail.com>
2025-05-25 22:28:25 +0300

    Edited final version 3

---
+++ Tests/WFSUITests/WFSUITests.swift
@@ -0,0 +1,32 @@
+//
+//  WFSUITests.swift
+//  WFSUITests
+//
+//  Created by Gerald Oluoch on 24/05/2025.
+//
+
+import XCTest
+@testable import WFSUI
+
+final class WFSUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+    func testExample() throws {
+
+    }
+
+    func testPerformanceExample() throws {
+        
+        self.measure {
+            
+        }
+    }
+
+}
